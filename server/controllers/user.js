5a7ffe17b66f80bc5e460b1ea323b8bf9184174e
zlsnowdo <zlsnowdo@ncsu.edu>
2023-01-07 23:17:26 -0500

    Finished backend auth

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,13 +1,13 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-import User from '../models/user.js';
+import UserModal from '../models/user.js';
 
 export const signin = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const existingUser = await User.findOne({ email });
+        const existingUser = await UserModal.findOne({ email });
         if(!existingUser) return res.status(404).json({ message: "User doesn't exist." });
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
         if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" } );
@@ -22,11 +22,11 @@ export const signup = async (req, res) => {
     const { email, password, confirmPassword, firstName, lastName } = req.body;
 
     try {
-        const existingUser = await User.findOne({ email });
+        const existingUser = await UserModal.findOne({ email });
         if(existingUser) return res.status(400).json({ message: "User already exist." });
         if(password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match" } );
         const hashedPassword = await bcrypt.hash(password, 12);
-        const result = await User.create({ email, hashedPassword, name:`${firstName} ${lastName}` });
+        const result = await UserModal.create({ email, password: hashedPassword, name:`${firstName} ${lastName}` });
         const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: "1hr" });
         res.status(200).json({ result: result, token: token });
     } catch (error) {
