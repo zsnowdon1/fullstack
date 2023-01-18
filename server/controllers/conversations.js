a3c57133fcced7b660f9a5ca1c2e702aef00fcb1
zlsnowdo <zlsnowdo@ncsu.edu>
2023-01-18 13:07:45 -0500

    Added getConversationById

--- server/controllers/conversations.js
+++ server/controllers/conversations.js
@@ -1,9 +1,22 @@
+import conversations from "../models/conversations.js";
 import Conversation from "../models/conversations.js";
+import Messages from "../models/messages.js";
 
 export const getConversations = async (req, res) => {
     try {
         const conversations = await Conversation.find();
         res.status(200).json(conversations);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getConversationById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const conversations = await Conversation.findById(id);
+        const messages = await Messages.find({conversationId: id});
+        res.status(200).json({conversation: conversations, messages: messages});
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
--- server/routes/conversations.js
+++ server/routes/conversations.js
@@ -1,12 +1,11 @@
 import express from 'express';
 
-import { getConversations, createConversation } from '../controllers/conversations.js';
+import { getConversations, createConversation, getConversationById } from '../controllers/conversations.js';
 
 const router = express.Router();
 
-// localhost:5000/posts
 router.get('/', getConversations);
 router.post('/', createConversation);
-//router.get('/:id', getPost);
+router.get('/:id', getConversationById);
 
 export default router;
